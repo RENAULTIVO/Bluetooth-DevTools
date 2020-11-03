fa7d133100003c7911f888b5c59e6681db775cd3
Renault Scabora <=>
2020-11-03 19:26:57 -0300

    Implement main screen

---
+++ app/src/main/java/com/renaultivo/bluetoothdevtools/MainActivity.java
@@ -0,0 +1,14 @@
+package com.renaultivo.bluetoothdevtools;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.main_activity);
+    }
+
+}
